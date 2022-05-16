d230916184ecacd8d16fb80329d3165ee4da9a34
Istalri Skolir Productions <47321091+JohnWeightman@users.noreply.github.com>
2022-05-16 22:07:21 +0100

    Comiit #2

--- LPG Game Configurator/Form1.cs
+++ LPG Game Configurator/Form1.cs
@@ -10,12 +10,21 @@ using System.Windows.Forms;
 
 namespace LPG_Game_Configurator
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
-        public Form1()
+        public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MenuStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
     }
 }
---
+++ LPG Game Configurator/Models/Game.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPG_Game_Configurator.Models
+{
+    class Game
+    {
+        #region Fields
+
+        private string name;
+        private string tagLine;
+        private string description;
+        private DateTime release;
+
+        #endregion
+
+        #region Get/Set Functions
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if(name != value)
+                {
+                    name = value;
+                }
+            }
+        }
+
+        public string TagLine
+        {
+            get { return tagLine; }
+            set
+            {
+                if(tagLine != value)
+                {
+                    tagLine = value;
+                }
+            }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                if(description != value)
+                {
+                    description = value;
+                }
+            }
+        }
+
+        public DateTime Release
+        {
+            get { return release; }
+            set
+            {
+                if(release != value)
+                {
+                    release = value;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Constructors/Destructors
+
+        public Game()
+        {
+
+        }
+
+        public Game (string setName, string setTagLine, string setDescription, DateTime setRelease)
+        {
+            name = setName;
+            tagLine = setTagLine;
+            description = setDescription;
+            release = setRelease;
+        }
+
+        #endregion
+    }
+}
