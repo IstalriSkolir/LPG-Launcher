e24ce4e3c5d585af48e779a835a9fd3c6dc7c251
Istalri Skolir Productions <47321091+JohnWeightman@users.noreply.github.com>
2022-05-20 18:25:41 +0100

    LPG Launcher #10
    #10
    - Build process will delete older launchers, stores 5 more recent build installers

--- LPG Launcher/VersionAutoIncrementer.cs
+++ LPG Launcher/VersionAutoIncrementer.cs
@@ -1,6 +1,6 @@
 ﻿
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.20.825")]
-[assembly: AssemblyFileVersion("0.0.20.825")]
+[assembly: AssemblyVersion("0.0.20.1041")]
+[assembly: AssemblyFileVersion("0.0.20.1041")]
 
