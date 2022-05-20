06a24381869c67d82aa400eb1d9c3cd6c581f647
Istalri Skolir Productions <47321091+JohnWeightman@users.noreply.github.com>
2022-05-20 22:13:11 +0100

    Misc #1
    - Added obj\Debug and obj\Release directories from both projects to .gitignore
    - Set Launcher.exe Icon to LogoTemp.ico

--- LPG Launcher/VersionAutoIncrementer.cs
+++ LPG Launcher/VersionAutoIncrementer.cs
@@ -1,6 +1,6 @@
 ﻿
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.20.1220")]
-[assembly: AssemblyFileVersion("0.0.20.1220")]
+[assembly: AssemblyVersion("0.0.20.1270")]
+[assembly: AssemblyFileVersion("0.0.20.1270")]
 
