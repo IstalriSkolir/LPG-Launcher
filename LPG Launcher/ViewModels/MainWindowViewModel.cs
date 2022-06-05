7bd1d52f7a5e70bd413f90737b2195881b46ec32
Istalri Skolir Productions <47321091+JohnWeightman@users.noreply.github.com>
2022-06-05 17:36:46 +0100

    Commits for #74 & #75
    - Added Sidebar Menu
    - Added About page (Still needs work)

--- LPG Launcher/ViewModels/MainWindowViewModel.cs
+++ LPG Launcher/ViewModels/MainWindowViewModel.cs
@@ -12,11 +12,16 @@ using LPG_Launcher.Models;
 
 namespace LPG_Launcher.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : INotifyPropertyChanged
     {
         #region Fields
 
+        public event PropertyChangedEventHandler PropertyChanged;
         private GameStorage gameData;
+        private ICommand showLibraryCommand;
+        private ICommand showAboutCommand;
+        private bool showLibrary;
+        private bool showAbout;
 
         #endregion
 
@@ -27,6 +32,42 @@ namespace LPG_Launcher.ViewModels
             get { return gameData; }
         }
 
+        public ICommand ShowLibraryCommand
+        {
+            get { return showLibraryCommand ?? (showLibraryCommand = new CommandHandler(() => showPanel("Library"), () => true)); }
+        }
+
+        public ICommand ShowAboutCommand
+        {
+            get { return showAboutCommand ?? (showAboutCommand = new CommandHandler(() => showPanel("About"), () => true)); }
+        }
+
+        public bool ShowLibrary
+        {
+            get { return showLibrary; }
+            set
+            {
+                if(showLibrary != value)
+                {
+                    showLibrary = value;
+                    onPropertyChange("showLibrary");
+                }
+            }
+        }
+
+        public bool ShowAbout
+        {
+            get { return showAbout; }
+            set
+            {
+                if (showAbout != value)
+                {
+                    showAbout = value;
+                    onPropertyChange("showAbout");
+                }
+            }
+        }
+
         #endregion
 
         #region Contructors/Destructors
@@ -35,6 +76,42 @@ namespace LPG_Launcher.ViewModels
         {
             gameData = new GameStorage(this);
             ToolTipService.ShowDurationProperty.OverrideMetadata(typeof(DependencyObject), new FrameworkPropertyMetadata(Int32.MaxValue));
+            showPanel("Library");
+        }
+
+        #endregion
+
+        #region Private Functionality
+
+        private void onPropertyChange(string pPropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(pPropertyName));
+            }
+        }
+
+        private void showPanel(string panel)
+        {
+            hideAllPanels();
+            switch (panel)
+            {
+                case "Library":
+                    ShowLibrary = true;
+                    break;
+                case "About":
+                    ShowAbout = true;
+                    break;
+                default:
+                    MessageBox.Show("Error Showing " + panel, "Panel Error", MessageBoxButton.OK);
+                    break;
+            }
+        }
+
+        private void hideAllPanels()
+        {
+            ShowLibrary = false;
+            ShowAbout = false;
         }
 
         #endregion
