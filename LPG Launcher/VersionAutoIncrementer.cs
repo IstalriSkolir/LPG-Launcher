12418102e0b96f92e082ee1fe92ec87b93f4e87c
Istalri Skolir Productions <47321091+JohnWeightman@users.noreply.github.com>
2022-07-02 13:42:21 +0100

    Commit for #79
    - Font now packaged with application

--- LPG Launcher/VersionAutoIncrementer.cs
+++ LPG Launcher/VersionAutoIncrementer.cs
@@ -1,6 +1,6 @@
 ﻿
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.2.2.728")]
-[assembly: AssemblyFileVersion("0.2.2.728")]
+[assembly: AssemblyVersion("0.2.2.759")]
+[assembly: AssemblyFileVersion("0.2.2.759")]
 
